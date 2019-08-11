04d31efcec52b87e6d980e8d4903ca8b61304acb
louis-mael <louis-mael@Lightning>
2019-08-11 12:13:20 +0200

    Update 11/08/19 - 12:12
    - Update Entity.java
    - Update Level0.java
    
    - Edit Level0
    - Prepare new level's structure

--- Cube 2/src/main/java/com/torpill/game/level/Level0.java
+++ Cube 2/src/main/java/com/torpill/game/level/Level0.java
@@ -16,8 +16,16 @@ public class Level0 extends Level {
 		super.init();
 
 		Block.create(StoneBlock.class, 0, 10, 60, 3).addTo(this);
+		Block.create(StoneBlock.class, 60, 10, 3, 9).addTo(this);
 		Block.create(StoneBlock.class, 70, 10, 20, 3).addTo(this);
+		Block.create(StoneBlock.class, 95, 17, 30, 3).addTo(this);
+		Block.create(StoneBlock.class, 133, 17, 5, 4).addTo(this);
+		Block.create(StoneBlock.class, 138, 17, 6, 1).addTo(this);
+		Block.create(StoneBlock.class, 144, 17, 5, 4).addTo(this);
+		Block.create(LavaBlock.class, 138, 18, 6, 2).addTo(this);
 		Block.create(LavaBlock.class, 0, 0, 500, 6).addTo(this);
+
+		Block.createDecoration(StoneBlock.class, 137, 17, 8, 1).addTo(this);
 	}
 
 	public String getName() {
